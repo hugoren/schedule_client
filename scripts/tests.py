b78ca75a197553afb0252574c19da5228e3e73e7
hugo <chenpengren@0easy.com>
2018-01-24 14:43:53 +0800

    rc

--- scripts/tests.py
+++ scripts/tests.py
@@ -2,12 +2,34 @@ import requests
 import json
 
 
-def func(target, file_name):
-    print(target, file_name)
-    return {"retcode": 0, "stdout": "Remote command completes."}
+def file_sync(target, file_name):
+    token = 'Schedule0350c8c75ddcd9fafdaA9738df4c9346bec48dc9c4915'
+    url = 'http://127.0.0.1:10011/api/v1/schedule/file_sync/'
+    data = {"target": target, "file_name": file_name}
+    r = requests.get(url, data=json.dumps(data),
+                 headers={'Content-Type': 'application/json', 'token': token}).json()
+    print(r)
 
 
 
+def remote_command():
+    target = '127.0.0.1'
+    command = 'command'
+    script_name = 'tests'
+    args = ('a', 'b')
+    kwargs = {"a": "a"}
+    token = 'Schedule0350c8c75ddcd9fafdaA9738df4c9346bec48dc9c4915'
+    url = 'http://127.0.0.1:10011/api/v1/schedule/command/'
+    data = {"fun_name": 'func', "command": command, "target": target, "script_name": script_name,
+            "args": args
+            }
+    r = requests.get(url, data=json.dumps(data),
+                 headers={'Content-Type': 'application/json', 'token': token}).json()
+    print(r)
 
 
+if __name__ == '__main__':
+    # remote_command()
+
+    file_sync(target="127.0.0.1", file_name="tests.py")
 
--- utils.py
+++ utils.py
@@ -29,7 +29,7 @@ def log(level, message):
 
 def file_sync(msg):
     try:
-        with open('{0}'.format(msg.get('file_name')), 'w') as f:
+        with open('scripts/{0}'.format(msg.get('file_name')), 'w') as f:
             f.writelines(msg.get('content'))
         send_data = {"jid": msg.get("jid"), "retcode": 0, "stdout": "{0} sync success".format(msg.get("file_name"))}
         log("info", "{0} 文件同步完成".format(msg.get('file_name')))
@@ -40,20 +40,3 @@ def file_sync(msg):
         log("error", "{0} 文件同步异常, {1}".format(msg.get('file_name'), e))
         return send_data
 
-
-def command(jid, cmd, user='su'):
-    timeout = 300
-    kill = lambda process: process.kill()
-    p = subprocess.Popen(args=[user, '-l', '-c', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
-    my_timer = Timer(timeout, kill, [p])
-    try:
-        out, err = None, None
-        my_timer.start()
-        pid = p.pid
-        code = p.wait()
-        out, err = p.communicate()
-    finally:
-        if my_timer.finished._Event__flag:
-            my_timer.cancel()
-            log('error', 'the {0} is waiting ,so close it'.format(pid))
-        return {"jid": jid, 'retcode': 0, 'stderr': err, 'stdout': out}
